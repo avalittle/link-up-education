80d569b4b05dc7ec06d3e9413b4f4b89afa1b26d
Brent Champion <brent.champion@queensu.ca>
2020-11-08 14:17:25 -0500

    test

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
 import Browse from './components/browse/Browse';
-import Coursepage from './components/coursepage/Coursepage'
+import Class from './components/class/Class';
 import Matches from './components/matches/matches';
 
 export default function Routes() {
@@ -10,7 +10,7 @@ export default function Routes() {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/browse" component={Browse} />
-            <Route exact path="/class/:id" component={Coursepage} />
+            <Route exact path="/class/:id" component={Class} />
             <Route exact path="/matches" component={Matches}/>
         </Switch>
     );
